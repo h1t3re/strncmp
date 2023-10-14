63ae3e51b530b7ecba5381ffab307fc38a51761d
h1t3re <143657823+h1t3re@users.noreply.github.com>
2023-10-14 02:02:54 +0200

    Update strncmp.c

--- strncmp.c
+++ strncmp.c
@@ -1,12 +1,12 @@
 int strncmp(long unsigned int size, const char *str1, const char *str2)
 {
 	int i = 0;
-	while(i < (size*32))
+	while(i < size)
 	{
 		if(*(&str1) != *(&str2))
 			return 1;
-		str1 = str1 +1;
-		str2 = str2 +1;	
+		str1 = str1 +32;
+		str2 = str2 +32;	
 		i = i +1;
 	}
 	i = 0;
