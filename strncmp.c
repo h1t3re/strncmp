1fb55ac0cf9015b9955fdfed25b2efb173e291a4
h1t3re <143657823+h1t3re@users.noreply.github.com>
2023-10-31 06:18:03 +0100

    Add files via upload

--- strncmp.c
+++ strncmp.c
@@ -1,7 +1,9 @@
 int strncmp(const char *str1, const char *str2)
 {
 	int i = 0;
-	while((*(str1+i) != '\0') | (*(str2+i) != '\0'))
+	if(((*(str1) != '\0') & (*(str2) == '\0')) & ((*(str1) == '\0') & (*(str2) != '\0')))
+		return 1;
+	while((*(str1+i) != '\0') & (*(str2+i) != '\0'))
 	{
 		if(*(str1+i) != *(str2+i))
 			return 1;
